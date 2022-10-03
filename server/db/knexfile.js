1556935e8cac3992b8b4e96f3945e0763b0f65b3
Brendan Jarvis <jarvisbrendanj@gmail.com>
2022-10-03 21:51:28 +1300

    Amend knexfile.js

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -3,57 +3,23 @@ const path = require('path')
 module.exports = {
   development: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
       filename: path.join(__dirname, 'dev.sqlite3'),
     },
-    migrations: {
-      directory: path.join(__dirname, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(__dirname, 'seeds'),
-    },
+    useNullAsDefault: true,
   },
 
   test: {
     client: 'sqlite3',
-    useNullAsDefault: true,
     connection: {
       filename: ':memory:',
     },
-    migrations: {
-      directory: path.join(__dirname, 'migrations'),
-    },
-    seeds: {
-      directory: path.join(__dirname, 'seeds'),
-    },
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+    useNullAsDefault: true,
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.PGHOST,
-      port: process.env.PGPORT,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
